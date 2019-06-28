10320bb26e647026971dfd9fb63ba1cebc325f6e
Sneha Kalita <sneha3950@gmail.com>
2019-06-28 22:50:21 +0530

    working on search features

--- main.js
+++ main.js
@@ -2,17 +2,22 @@ function loadInfo(){
     event.preventDefault();
 
     var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    var responseObject = JSON.parse(this.responseText);
-                    movie(responseObject.Search);
-                }
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var responseObject = JSON.parse(this.responseText);
+            if (responseObject.Response == 'True') {
+                movie(responseObject.Search);
+            } else {
+                document.getElementById('filmsList').innerHTML = "<p>Movie not found</p>";
             }
-                    xhttp.open("GET", "https://www.omdbapi.com/?apikey=cdf2dbb2&s=Batman&page=2", true);
-                    xhttp.send();
+        }
+    }
+    var search = document.getElementById('search').value;
+    xhttp.open("GET", "https://www.omdbapi.com/?apikey=cdf2dbb2&s=" + search, true);
+    xhttp.send();
 }
 
-function movie(movieList){
+function movie(movieList) {
     let movieListHtml = '';
     //creating a varaible of string with blank value
     movieList.map(item => {
